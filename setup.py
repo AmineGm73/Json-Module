52730dc051d825a16c96fa174f00b21214e0e7d0
AmineGm73 <aminegazit30@gmail.com>
2024-03-23 15:26:18 +0100

    V0.1.0

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='json_m',
+    version='0.1.0',
+    description='A useful tool to use json as database',
+    packages=find_packages(),
+    author='Your Name',
+    author_email='aminegazit30@gmail.comAmineGM',
+    install_requires=[],
+)
