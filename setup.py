d2b36d7bafd4db862fd00ef20549d6ebf0eb37e9
AmineGm73 <124823874+AmineGm73@users.noreply.github.com>
2024-10-07 19:20:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     description='A useful tool to use json as database',
     packages=find_packages(),
-    author='Your Name',
+    author='Amine Dev',
     author_email='aminegazit30@gmail.comAmineGM',
     install_requires=[],
 )
